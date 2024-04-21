77726565d79ce945f2b2489dd644374788f2cb4f
msajiba <asajib499@gmail.com>
2024-04-21 12:34:16 +0600

    testing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,7 +8,7 @@ const Main = () => {
 
     // const dummyURL = "https://drive.google.com/file/d/17mM8E6JZwEBAvS8ZUKQk3Ns9Z1x8UF8Y/view?usp=drive_web"
 
-    const [url, setUrl] = useState<string>("");
+    const [url, setUrl] = useState<string>("https://drive.google.com/file/d/17mM8E6JZwEBAvS8ZUKQk3Ns9Z1x8UF8Y/view?usp=drive_web");
 
     return (
         <div>
